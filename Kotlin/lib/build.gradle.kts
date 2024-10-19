3f9cf6a965e549335d03ebe06e1dbce1570a7fff
Esoteric Enderman <esotericenderman@gmail.com>
2024-10-19 19:09:32 +0100

    Use `esotericenderman` as it is more OG

--- Kotlin/lib/build.gradle.kts
+++ Kotlin/lib/build.gradle.kts
@@ -57,7 +57,7 @@ publishing {
 
                 developers {
                     developer {
-                        id.set("esoteric-enderman")
+                        id.set("esotericenderman")
                         name.set("Esoteric Enderman")
                         email.set("your.email@example.com")
                     }
