28c18b59db5c82c2c5db81ed28024b7d5bbafcb4
Esoteric Enderman <esotericenderman@gmail.com>
2024-10-19 19:08:27 +0100

    Configure the project to release to Maven central
    This code was written by ChatGPT.

--- Kotlin/lib/build.gradle.kts
+++ Kotlin/lib/build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
     alias(libs.plugins.jvm)
-
     `java-library`
+    `maven-publish` // Add maven-publish plugin
+    signing          // Add signing plugin
 }
 
 group = "dev.enderman"
@@ -17,7 +18,6 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
     api(libs.commons.math3)
-
     implementation(libs.guava)
 }
 
@@ -33,4 +33,59 @@ tasks.named<Jar>("jar") {
 
 tasks.named<Test>("test") {
     useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            groupId = "dev.enderman"
+            artifactId = rootProject.name
+            version = project.version.toString()
+
+            pom {
+                name.set(rootProject.name)
+                description.set("Your project description here")
+                url.set("https://your.project.url") // Update with your project URL
+
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("esoteric-enderman")
+                        name.set("Esoteric Enderman")
+                        email.set("your.email@example.com")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/your_username/your_project.git")
+                    developerConnection.set("scm:git:ssh://github.com:your_username/your_project.git")
+                    url.set("https://github.com/your_username/your_project")
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "OSSRH"
+            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+
+            credentials {
+                username = project.findProperty("ossrhUsername") as String? ?: ""
+                password = project.findProperty("ossrhPassword") as String? ?: ""
+            }
+        }
+    }
+}
+
+signing {
+    useGpgCmd() // Use GPG for signing the artifacts
+    sign(publishing.publications["mavenJava"])
 }
