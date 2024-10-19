601c0a1d3168c0d6850a7855e1ef9cac1affdc71
Esoteric Enderman <esotericenderman@gmail.com>
2024-10-19 16:22:40 +0100

    Add math object test

---
+++ Kotlin/lib/src/test/kotlin/dev/enderman/math/MathObjectTest.kt
@@ -0,0 +1,48 @@
+package dev.enderman.math
+
+import dev.enderman.math.objects.MathObject
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
+
+class MathObjectTest {
+    @Test fun someLibraryMethodReturnsTrue() {
+        val myListA = MyList(1, 2)
+
+        assertEquals(myListA, myListA, "MathObject equality is reflexive")
+
+        val a: MyList? = null
+
+        assertNotEquals(a, myListA, "MathObject instances are never equal to null")
+
+        val myListB = MyList(1, 2)
+
+        assertEquals(myListA, myListB, "MathObject equality should return 'true' when object values are equal")
+
+        val myListC = MyList(42, 0)
+        val myListD = myListC.clone()
+
+        assertEquals(myListD, myListC, "MathObject equality should return 'true' after cloning")
+    }
+
+    private class MyList(a: Int, b: Int) : MathObject {
+
+        val array: List<Int> = listOf(a, b);
+
+        override fun clone(): MyList {
+            return MyList(array[0], array[1])
+        }
+
+        override fun equals(other: Any?): Boolean {
+            if (super.equals(other)) {
+                return true
+            }
+
+            if (other !is MyList) {
+                return false
+            }
+
+            return other.array == array
+        }
+    }
+}
